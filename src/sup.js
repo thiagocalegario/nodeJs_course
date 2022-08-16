e0dfab08d354f6fc00b78bcff9f68345baa0f1ab
Thiago - IBM <IBM@MacBook-Pro-de-Thiago.local>
2022-08-16 10:27:54 -0300

    change the sup directory

---
+++ src/sup.js
@@ -0,0 +1,33 @@
+const request = require("request");
+
+const getCoords = (city, callback ) => {
+    const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + city + ".json?proximity=ip&types=place%2Cpostcode%2Caddress&access_token=pk.eyJ1IjoidGhpYWdvY2FsZWdhcmlvIiwiYSI6ImNsNm1jbmFtajBrYW0zZG10aXBycnU2YWoifQ.spFuCwC5RHuPrA8YG5-4pw"
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined, undefined)
+        } else if (body.features.length === 0) {
+            callback('Unable to find location. Try another search.', undefined, undefined)
+        } else {
+            callback(undefined, {
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0]
+            })
+        }
+    })
+}
+const printConditions = (coord2, coord1, callback)  =>{
+    const url = "http://api.weatherstack.com/current?access_key=7ac710f5433ce63d483701b3fcc255f9&query=" + coord2 + "," +coord1
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, "It is "+ body.current.temperature + " degress and " + body.current.weather_descriptions)
+        }
+    })
+}
+module.exports = {
+    getCoords,
+    printConditions
+}
